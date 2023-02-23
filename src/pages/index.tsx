fc504791b0d97f07254dce1786eaa57ce5f547b3
Bruna Flor <brn.flor@gmail.com>
2023-02-23 18:31:55 +0000

    wip: getting prismic  to home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,6 +17,10 @@ interface Post {
   };
 }
 
+interface Posts {
+  posts: Post[];
+}
+
 interface PostPagination {
   next_page: string;
   results: Post[];
@@ -26,11 +30,29 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
+export default function Home({ posts }: Posts) {
   return (
     <main className={`${styles.homeContainer} ${commonStyles.container}`}>
       <div>
-        <Link href="#">
+        {posts?.map(post => {
+          return (
+            <Link href="#">
+              <strong>{post.data.title}</strong>
+              <p>{post.data.subtitle}</p>
+              <div>
+                <span>
+                  <FiCalendar />
+                  {post.first_publication_date}
+                </span>
+                <span>
+                  <FiUser />
+                  {post.data.author}
+                </span>
+              </div>
+            </Link>
+          );
+        })}
+        {/* <Link href="#">
           <strong>Title</strong>
           <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis
@@ -48,36 +70,28 @@ export default function Home() {
               Adriana Calcanhoto
             </span>
           </div>
-        </Link>
-
-        <Link href="#">
-          <strong>Title</strong>
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis
-            quidem voluptates voluptatum perferendis tempora fugit eos similique
-            voluptas, est doloribus quod inventore maxime nulla autem laudantium
-            qui, suscipit, ad excepturi!
-          </p>
-          <div>
-            <span>
-              <FiCalendar />
-              19 abr 2021
-            </span>
-            <span>
-              <FiUser />
-              Adriana Calcanhoto
-            </span>
-          </div>
-        </Link>
+        </Link> */}
       </div>
       <a className={styles.loadMore}>Carregar mais posts</a>
     </main>
   );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+export const getStaticProps = async () => {
+  const prismic = getPrismicClient({});
+  const postsResponse = await prismic.getByType('posts', { pageSize: 20 });
 
-//   // TODO
-// };
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  return { props: { posts } };
+};
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,5 +1,7 @@
 // import { GetStaticPaths, GetStaticProps } from 'next';
 
+import { getPrismicClient } from '../../services/prismic';
+
 // import { getPrismicClient } from '../../services/prismic';
 
 // import commonStyles from '../../styles/common.module.scss';
@@ -38,9 +40,23 @@ export default function Post() {
 //   // TODO
 // };
 
-// export const getStaticProps = async ({params }) => {
-//   const prismic = getPrismicClient({});
-//   const response = await prismic.getByUID(TODO);
+export const getStaticProps = async ({ params }) => {
+  // const prismic = getPrismicClient({});
+  // const response = await prismic.getByUID(TODO);
 
-//   // TODO
-// };
+  // // TODO
+
+  const prismic = getPrismicClient({});
+  const postsResponse = await prismic.getByType('posts', { pageSize: 20 });
+
+  const post = postsResponse.results.map(post => {
+    return {
+      slug: post.uid,
+      title: post.data.title,
+      subtitle: post.data.subtitle,
+      author: post.data.author,
+      banner: post.data.banner,
+      content: post.data.content,
+    };
+  });
+};
