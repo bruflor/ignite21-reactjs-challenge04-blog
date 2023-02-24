9e30a1de097c85cfb3396b83d17f4307ddbe2632
Bruna Flôr <brn.flor@gmail.com>
2023-02-24 18:28:19 +0000

    wip: getting post

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,11 +1,10 @@
-// import { GetStaticPaths, GetStaticProps } from 'next';
+import { GetStaticPaths, GetStaticProps } from 'next';
 
+import { useRouter } from 'next/router';
 import { getPrismicClient } from '../../services/prismic';
 
-// import { getPrismicClient } from '../../services/prismic';
-
-// import commonStyles from '../../styles/common.module.scss';
-// import styles from './post.module.scss';
+import commonStyles from '../../styles/common.module.scss';
+import styles from './post.module.scss';
 
 interface Post {
   first_publication_date: string | null;
@@ -29,34 +28,63 @@ interface PostProps {
 }
 
 export default function Post() {
-  // TODO
-  return <h1>Post</h1>;
+  const router = useRouter();
+  return (
+    <>
+      <div>{router.isFallback ? 'banana' : 'jacaré'}</div>
+    </>
+  );
 }
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient({});
-//   const posts = await prismic.getByType(TODO);
-
-//   // TODO
-// };
-
-export const getStaticProps = async ({ params }) => {
-  // const prismic = getPrismicClient({});
-  // const response = await prismic.getByUID(TODO);
-
-  // // TODO
-
+export const getStaticPaths = async () => {
   const prismic = getPrismicClient({});
-  const postsResponse = await prismic.getByType('posts', { pageSize: 20 });
-
-  const post = postsResponse.results.map(post => {
+  const posts = await prismic.getByType('posts');
+  const paths = posts.results.map(post => {
     return {
-      slug: post.uid,
-      title: post.data.title,
-      subtitle: post.data.subtitle,
-      author: post.data.author,
-      banner: post.data.banner,
-      content: post.data.content,
+      params: {
+        slug: post.uid,
+      },
     };
   });
+
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const { uid } = params;
+  const prismic = getPrismicClient({});
+  const response = await prismic.getByUID('posts', uid as string);
+
+  // const prismic = getPrismicClient({});
+  const post = response;
+  // const post = response.results.map(post => {
+  //   return {
+  //     first_publication_date: post.first_publication_date,
+  //     data: {
+  //       title: post.data.title,
+  //       banner: {
+  //         url: post.data.banner,
+  //       },
+  //       author: post.data.author,
+  //       content: {
+  //         heading: post.data.content.heading,
+  //         body: {
+  //           text: post.data.content.body.text,
+  //         },
+  //       },
+  //     },
+  //   };
+  // });
+
+  console.log(response);
+
+  return {
+    props: {
+      post,
+    },
+    revalidate: 1,
+  };
 };
